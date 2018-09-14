44be671fef60a79cdef835f653b0a8ddd2b49bcf
b0b123 <s.voogd@st.hanze.nl>
2018-09-14 11:18:49 +0200

    empty grid start

---
+++ Week1/Opgave2.py
@@ -0,0 +1,14 @@
+class Boggle:
+
+    def makeBoard(board_size):
+        board = []
+        for i in range(board_size):
+            board.append([] * board_size)
+
+        return board
+
+
+
+    board = [5][5]
+
+    print(board)
