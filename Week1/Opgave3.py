bd2324c533759d3810ca29516fdbdad6a04608d0
qwerty12794 <jhofman3@gmail.com>
2018-09-17 10:36:17 +0200

    dit slaat nergens op

--- Week1/Opgave3.py
+++ Week1/Opgave3.py
@@ -1,3 +1,14 @@
+class Board:
+    @staticmethod
+    def print_board(board):
+        # print('[%s]' % ', '.join(map(str, board)))
+        for row in board:
+            print(row)
+
+
 class Main:
     def main(self):
-        None
+        board = [[1, 0, 0], [0, 5, 0], [0, 0, 9]]
+        # board = Board.board
+        Board.print_board(board)
+
