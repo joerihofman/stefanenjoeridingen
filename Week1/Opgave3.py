81dab7e56ffd11bf151f9b42d11f500bb2e63746
qwerty12794 <jhofman3@gmail.com>
2018-09-17 11:21:32 +0200

    ja

--- Week1/Opgave3.py
+++ Week1/Opgave3.py
@@ -4,7 +4,6 @@ class Board:
         self.board = [[1, 0, 0], [0, 5, 0], [0, 0, 9]]
         self.clues = []
 
-
     def print_board(self):
         for row in self.board:
             print(' '.join([str(letter) for letter in row]))
@@ -29,21 +28,33 @@ class Board:
                     self.clues.append(self.board[row][col])
         return self.clues
 
+    def get_neighbors(self, row, col):
+        neighbors = []
+        length = len(self.board) - 1
 
-
+        positions = [(row-1, col), (row, col-1), (row, col+1), (row+1, col)]
+        for pos in positions:
+            row = pos[0]
+            col = pos[1]
+            if row < 0:
+                row = length
+            elif row > length:
+                row = 0
+            if col < 0:
+                col = length
+            elif col > length:
+                col = 0
+            neighbors.append((row, col))
+        return neighbors
 
 
 class Main:
     @staticmethod
     def main():
         board = Board()
-        board.get_clues()
-
-
         board.print_board()
 
-        board.change_number(0, 0, 3)
-        board.print_board()
+        print(board.get_neighbors(*board.get_postition(1)))
 
 
 Main.main()
