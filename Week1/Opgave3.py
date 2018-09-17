07d07286f080dd549440eb8afa1e81fb1cbc65fa
b0b123 <s.voogd@st.hanze.nl>
2018-09-17 11:16:08 +0200

    get_clues + change_number veranderingen

--- Week1/Opgave3.py
+++ Week1/Opgave3.py
@@ -2,15 +2,17 @@ class Board:
 
     def __init__(self):
         self.board = [[1, 0, 0], [0, 5, 0], [0, 0, 9]]
+        self.clues = []
+
 
     def print_board(self):
         for row in self.board:
             print(' '.join([str(letter) for letter in row]))
         print('')
 
-    def change_number(self, x, y, new_value):
-        if self.board[x][y] == 0:
-            self.board[x][y] = new_value
+    def change_number(self, row, col, new_value):
+        if self.board[row][col] not in self.clues:
+            self.board[col][row] = new_value
             return True
         return False
 
@@ -20,11 +22,28 @@ class Board:
                 if self.board[row][col] == number:
                     return row, col
 
+    def get_clues(self):
+        clues = []
+        for row in range(len(self.board)):
+            for col in range(len(self.board)):
+                if self.board[row][col] != 0:
+                    self.clues.append(self.board[row][col])
+        return clues
+
+
+
+
 
 class Main:
     @staticmethod
     def main():
         board = Board()
+        board.get_clues()
+
+
+        board.print_board()
+
+        board.change_number(0, 1, 3)
         board.print_board()
 
 
