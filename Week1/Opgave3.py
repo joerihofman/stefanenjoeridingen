84b5fa54d0841fb38115c6921f025c4ce29214af
qwerty12794 <jhofman3@gmail.com>
2018-09-17 10:37:17 +0200

    dit slaat nergens op

--- Week1/Opgave3.py
+++ Week1/Opgave3.py
@@ -1,7 +1,6 @@
 class Board:
     @staticmethod
     def print_board(board):
-        # print('[%s]' % ', '.join(map(str, board)))
         for row in board:
             print(row)
 
@@ -9,6 +8,5 @@ class Board:
 class Main:
     def main(self):
         board = [[1, 0, 0], [0, 5, 0], [0, 0, 9]]
-        # board = Board.board
         Board.print_board(board)
 
