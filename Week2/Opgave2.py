fcba460c5243b54a8b248cf44069c65782335cbb
qwerty12794 <jhofman3@gmail.com>
2018-09-24 17:29:35 +0200

    opgave 2 D is klaar, en er is een refactor geweest om het in classes te zetten

--- Week2/Opgave2.py
+++ Week2/Opgave2.py
@@ -1,121 +1,155 @@
 import random
 
-possible_moves = ['roll', 'hold']
-goal_score = 100
-victories = [0, 0]
-gamecounter = 0
+
+class Players:
+    current_score = 0
+    victories = 0
+
+    def __init__(self, name):
+        self.name = name
+
+    def reset_score(self):
+        self.current_score = 0
+
+    def add_to_victories(self):
+        self.victories += 1
+
+    def add_to_score(self, score):
+        self.current_score += score
+
+    def add_one(self):
+        self.current_score += 1
+
+
+class GameState:
+    goal_score = 100
+
+    pending_score = 0
+
+    game_counter = 0
+    hold_at_20 = 20
+    hold_at_x_other = 10
+
+    def __init__(self, me, you):
+        self.me = me
+        self.you = you
+        self.current_player = me
+
+    def change_player(self):
+        if self.current_player == self.me:
+            self.current_player = self.you
+        elif self.current_player == self.you:
+            self.current_player = self.me
+
+    def game_to_list(self):
+        return [self.me.current_score, self.you.current_score, self.pending_score]
+
+    def reset_scores(self):
+        self.game_counter += 1
+        self.me.reset_score()
+        self.you.reset_score()
+        self.pending_score = 0
+        self.change_player()
+
+    def add_to_score(self):
+        if self.current_player == self.me:
+            self.me.add_to_score(self.pending_score)
+        elif self.current_player == self.you:
+            self.you.add_to_score(self.pending_score)
+
+    def clear_pending_score(self):
+        self.pending_score = 0
+
+    def rolled_one(self):
+        if self.current_player == self.me:
+            self.me.add_one()
+            self.clear_pending_score()
+            self.change_player()
+        elif self.current_player == self.you:
+            self.you.add_one()
+            self.clear_pending_score()
+            self.change_player()
+
+    def check_victory(self):
+        if self.me.current_score >= self.goal_score:
+            print(self.game_to_list().__str__() + "  me heeft gewonnen")
+            self.me.add_to_victories()
+            self.reset_scores()
+
+        elif self.you.current_score >= self.goal_score:
+            print(self.game_to_list().__str__() + "  you heeft gewonnen")
+            self.you.add_to_victories()
+            self.reset_scores()
+
+
+class Moves:
+    @staticmethod
+    def roll(game, d):
+        if d == 1:
+            game.rolled_one()
+        elif d > 1:
+            game.pending_score += d
+
+    @staticmethod
+    def hold(game):
+        game.add_to_score()
+        game.clear_pending_score()
+        game.change_player()
+
+    @staticmethod
+    def hold_at_x(game, hold):
+        if game.pending_score >= hold:
+            return True
+
+
+class Strategies:
+    @staticmethod
+    def roll_dice():
+        roll_value = random.randint(1, 6)
+        return roll_value
+
+    @staticmethod
+    def clueless(game):
+        possible_moves = ['roll', 'hold']
+        random_move = random.SystemRandom().choice(possible_moves)
+        if random_move == "roll":
+            if Moves.hold_at_x(game, game.hold_at_20):
+                print(game.current_player.__str__() + " has chosen hold all by itself")
+                Moves.hold(game)
+            else:
+                print(game.current_player.__str__() + " has chosen roll")
+                Moves.roll(game, Strategies.roll_dice())
+        elif random_move == "hold":
+            print(game.current_player.__str__() + " has chosen hold")
+            Moves.hold(game)
+
+    @staticmethod
+    def play_smart(game):
+        game.check_victory()
+        if game.current_player == game.me:
+            if Moves.hold_at_x(game, game.hold_at_x_other):
+                Moves.hold(game)
+            else:
+                Moves.roll(game, Strategies.roll_dice())
+        if game.current_player == game.you:
+            if Moves.hold_at_x(game, game.hold_at_20):
+                Moves.hold(game)
+            else:
+                Moves.roll(game, Strategies.roll_dice())
+
+
+new_game = GameState(Players("me"), Players("you"))
+
+while new_game.game_counter < 100:
+    Strategies.play_smart(new_game)
+print("me heeft " + new_game.me.victories.__str__() + " keer gewonnen, en you " + new_game.you.victories.__str__())
+
 
 '''
-    State is a namedtuple of (p, me, you, pending) where
-    p: 'me' or 'you', indicating which player's turn it is
-    me: my total score
-    you: your total score
-    pending: the number of pending points (accumulated on current turn)
+opgave c: hold at 20 is veel beter; die wint meer dan 2/3 keer
+
+opgave d: de verwachte worp is gemiddeld 3,5; (1+2+3+4+5+6)/6
+            je kan ongeveer 5x gooien voor dat je een 1 krijgt
+            dus 5*3,5 = 17,5
 '''
-game_state = ["me", 0, 0, 0]
-
-
-# A strategy that ignores the state and chooses at random from possible moves.
-def clueless(state):
-    random_move = random.SystemRandom().choice(possible_moves)
-    if random_move == "roll":
-        if hold_at_x(state):
-            print(state.__str__() + " has chosen hold all by itself")
-            hold(state)
-        else:
-            print(state.__str__() + " has chosen roll")
-            roll(state, roll_dice())
-    elif random_move == "hold":
-        print(state.__str__() + " has chosen hold")
-        hold(state)
-
-
-def just_play(state):
-    check_victory(state)
-    if state[0] == "me":
-        if hold_at_x(state, 10):
-            hold(state)
-        else:
-            roll(state, roll_dice())
-    if state[0] == "you":
-        if hold_at_x(state, 20):
-            hold(state)
-        else:
-            roll(state, roll_dice())
-
-
-'''
-    Apply a die roll d to yield a new state:
-    If d == 1, it's a pig-out, get 1 point (losing any accumulated 'pending' points),
-    and it is the other player's turn.
-    If d > 1, add d to 'pending' points.
-'''
-def roll(state, d):
-    if d == 1:
-        if state[0] == "me":
-            new_score = state[1] + d
-            state[1] = new_score
-            state[3] = 0
-            state[0] = "you"
-        elif state[0] == "you":
-            new_score = state[2] + d
-            state[2] = new_score
-            state[3] = 0
-            state[0] = "me"
-    elif d > 1:
-        score = state[3] + d
-        state[3] = score
-
-
-'''
-    Apply a hold to yield a new state: add the pending points to my total points and
-    it becomes the other player's turn.
-'''
-def hold(state):
-    if state[0] == "me":
-        new_score = state[3] + state[1]
-        state[1] = new_score
-        state[3] = 0
-        state[0] = "you"
-        return tuple(state)
-    elif state[0] == "you":
-        new_score = state[3] + state[2]
-        state[2] = new_score
-        state[3] = 0
-        state[0] = "me"
-        return tuple(state)
-
-
-def hold_at_x(state, hold):
-    if state[3] >= hold:
-        return True
-
-
-def check_victory(state):
-    global gamecounter
-    global game_state
-    if state[1] >= goal_score:
-        print(state.__str__() + "  me heeft gewonnen")
-        victories[0] += 1
-        game_state = ["you", 0, 0, 0]
-        gamecounter += 1
-
-    elif state[2] >= goal_score:
-        print(state.__str__() + "  you heeft gewonnen")
-        victories[1] += 1
-        game_state = ["me", 0, 0, 0]
-        gamecounter += 1
-
-
-def roll_dice():
-    roll_value = random.randint(1, 6)
-    return roll_value
-
-
-while gamecounter < 100:
-    just_play(game_state)
-print(victories)
-
-#opgave c: hold at 20 is veel beter; die wint meer dan 2/3 keer
-
 
