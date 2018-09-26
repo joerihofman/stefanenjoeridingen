dd287bf78f87e7b9d6f8d859fb976ed26d651d1b
b0b123 <s.voogd@st.hanze.nl>
2018-09-26 23:20:52 +0200

    dingen

--- Week2/tsp_start.py
+++ Week2/tsp_start.py
@@ -4,6 +4,9 @@ import time
 import itertools
 import math
 from collections import namedtuple
+import sys
+
+sys.setrecursionlimit(100000)
 
 # Based on Peter Norvig's IPython Notebook on the TSP
 
@@ -67,83 +70,142 @@ def neirest_neighbors(cities, path=None, start=None):
         return neirest_neighbors(cities, path[:], start)
 
 
-def lineIntersection(pointA, pointB, pointC, pointD):
+# def lineIntersection(pointA, pointB, pointC, pointD):
+#
+#     print(pointA)
+#     print(pointB)
+#     print(pointC)
+#     print(pointD)
+#
+#     a1 = pointB.y - pointA.y
+#     b1 = pointA.x - pointB.x
+#     c1 = a1*(pointA.x) + b1*(pointA.y)
+#
+#     a2 = pointD.y - pointC.y
+#     b2 = pointC.x - pointD.x
+#     c2 = a2*(pointC.x) + b2*(pointD.y)
+#
+#     determinant = (a1*b2) - (a2*b1)
+#     print(determinant)
+#
+#     if determinant < 0:
+#         print('reeeeeeeeeeeeeeeeeeeeeeee')
+#     else:
+#
+#         print('er is een intersectie')
 
-    print(pointA)
-    print(pointB)
-    print(pointC)
-    print(pointD)
-
-    a1 = pointB.y - pointA.y
-    b1 = pointA.x - pointB.x
-    c1 = a1*(pointA.x) + b1*(pointA.y)
-
-    a2 = pointD.y - pointC.y
-    b2 = pointC.x - pointD.x
-    c2 = a2*(pointC.x) + b2*(pointD.y)
-
-    determinant = (a1*b2) - (a2*b1)
-    print(determinant)
-
-    if determinant < 0:
-        print('reeeeeeeeeeeeeeeeeeeeeeee')
-    else:
-
-        print('er is een intersectie')
 
 
-
-tour = neirest_neighbors(make_cities(10))
+tour = neirest_neighbors(make_cities(100))
 
 
 
 
 # lineIntersection(tour[0], tour[1], tour[8], tour[9])
 
+#
+# def line(a, b):
+#     A = (a.y - b.y)
+#     B = (b.x - a.x)
+#     C = ((a.x*b.y) - (b.x * a.y))
+#     return A, B, -C
 
-def line(a, b):
-    A = (a.y - b.y)
-    B = (b.x - a.x)
-    C = ((a.x*b.y) - (b.x * a.y))
-    return A, B, -C
-
-
-def intersection(L1, L2):
-    D  = L1[0] * L2[1] - L1[1] * L2[0]
-    Dx = L1[2] * L2[1] - L1[1] * L2[2]
-    Dy = L1[0] * L2[2] - L1[2] * L2[0]
-    if D != 0:
-        x = Dx / D
-        y = Dy / D
-
-
-        return x,y
-    else:
-        return False
+#
+# def intersection(L1, L2):
+#     D  = L1[0] * L2[1] - L1[1] * L2[0]
+#     Dx = L1[2] * L2[1] - L1[1] * L2[2]
+#     Dy = L1[0] * L2[2] - L1[2] * L2[0]
+#     if D != 0:
+#         x = Dx / D
+#         y = Dy / D
+#
+#
+#         return x,y
+#     else:
+#         return False
 
 def intersection(pA, pB, pC, pD):
 
-    def line(a, b):
-        A = (a.y - b.y)
-        B = (b.x - a.x)
-        C = ((a.x*b.y) - (b.x * a.y))
-        return A, B, -C
+    variables = [pA, pB, pC, pD]
+    if len(set(variables)) == len(variables):
 
-    line1 = line(pA, pB)
-    line2 = line(pC, pD)
 
-    D  = line1[0] * line2[1] - line1[1] * line2[0]
-    Dx = line1[2] * line2[1] - line1[1] * line2[2]
-    Dy = line1[0] * line2[2] - line1[2] * line2[0]
-    if D != 0:
-        x = Dx / D
-        y = Dy / D
-        if pA.x <= x <= pB.x or pB.x <= x <= pA.x and pC.y <= y <= pD.y or pD.y <= y <= pC.y:
-            print('er is een crossing hooooor')
-            return x,y
+        def line(a, b):
+            A = (a.y - b.y)
+            B = (b.x - a.x)
+            C = ((a.x*b.y) - (b.x * a.y))
+            return A, B, -C
+
+        line1 = line(pA, pB)
+        line2 = line(pC, pD)
+
+        D  = line1[0] * line2[1] - line1[1] * line2[0]
+        Dx = line1[2] * line2[1] - line1[1] * line2[2]
+        Dy = line1[0] * line2[2] - line1[2] * line2[0]
+        if D != 0:
+            x = Dx / D
+            y = Dy / D
+            # if pA.x <= x <= pB.x or pB.x <= x <= pA.x and pC.y <= y <= pD.y or pD.y <= y <= pC.y \
+            #         and pA.y <= y <= pB.y or pB.y <= x <= pA.y and pC.x <= x <= pD.x or pD.x <= x <= pC.x:
+            # print(pA)
+            # print(pB)
+            # print(pC)
+            # print(pD)
+            #
+            # print(x, y)
+            # print(pA.x <= x <= pB.x)
+            # print(pC.x >= x >= pD.x)
+            # print(pA.y <= y <= pB.y)
+            # print(pC.y <= y <= pD.y)
+            #
+            # print(line1)
+            # print(line2)
+            if min(pA.x, pB.x) <= x <= max(pB.x, pA.x) and min(pC.x, pD.x) <= x <= max(pD.x, pC.x) and min(pA.y, pB.y) <= y <= max(pB.y, pA.y) and min(pC.y, pD.y) <= y <= max(pD.y, pC.y):
+
+
+
+                # print('er is een crossing hooooor')
+                return True
+        else:
+            # print('lijnen lopen paralel')
+            return False
     else:
-        print('lijnen lopen paralel')
+        # print('waarden met de zelfde punten gevonden')
         return False
+
+
+
+
+# for i in range(len(tour)):1
+#     print('line1', tour[i], tour[i-1])
+#     for j in range(len(tour)):
+#         if tour[i]
+
+
+
+def rc(tour):
+    changes = False
+    nr = 0
+    for i in range(len(tour)):
+        for j in range(len(tour)):
+            # print(j)
+            if intersection(tour[i], tour[i-1], tour[j], tour[j-1]):
+                # temptour = tour[i-1]
+                nr += 1
+            # temptour2 = tour[i-1]
+            #     tour[i-1] = tour[j-1]
+            #     tour[j-1] = temptour
+            #     changes = True
+    if changes == True:
+        return rc(tour)
+    else:
+        print(nr)
+        return tour
+
+
+
+
+
 
 
 
@@ -167,11 +229,21 @@ def plot_tsp(algorithm, cities):
     plot_tour(tour)
 
 
+plot_tour(tour)
+
+rc(tour)
+
+
+
+
+
+# plot_tour(tour)
+
 
 
 # plot_tsp(neirest_neighbors, make_cities(100))
 
-plot_tsp(neirest_neighbors, make_cities(10))
+# plot_tsp(neirest_neighbors, make_cities(10))
 '''
 a)
 seed = 66
