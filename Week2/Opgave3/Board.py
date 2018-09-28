5e112e785194781a8a823965419f779dcb551596
qwerty12794 <jhofman3@gmail.com>
2018-09-28 13:46:49 +0200

    fout

--- Week2/Opgave3/Board.py
+++ Week2/Opgave3/Board.py
@@ -17,7 +17,7 @@ class Board:
         self.board[6][5] = Tile.WHITE
 
     def make_move(self, x, y):
-        if BoardHelper().is_legal_move(self.board, x, y):
+        if BoardHelper().is_legal_direction(self.board, x, y):
             self.board[x][y] = self.current_player
             self.other_player_turn()
 
@@ -34,12 +34,7 @@ class Board:
 
 
 class BoardHelper:
-    @staticmethod
-    def calculate_direction(original, new):
-        x_direction = int(original[0]) - int(new[0])
-        y_direction = int(original[1]) - int(new[1])
-
-        return x_direction, y_direction
+    directions = [(-1, 0), (0, -1), (0, 1), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]
 
     @staticmethod
     def get_possible_moves_considering_opponent(board, current_player, opponent):
@@ -51,21 +46,15 @@ class BoardHelper:
                 if column == current_player:
                     current_player_stones.append((board.index(row), row.index(column)))
 
-        for stone in current_player_stones:
-            direct_opponent_neighbors.append(BoardHelper.get_opponent_neighbors(board, stone[0], stone[1], opponent))
+        BoardHelper.do_things_for_directions(board, current_player_stones, opponent)
+
+        # for stone in current_player_stones:
+        #     direct_opponent_neighbors.append(BoardHelper.get_opponent_neighbors(board, stone[0], stone[1], opponent))
 
         # for i in range(len(current_player_stones)):
-        #     stone = current_player_stones[i]
-        #     for opponents in direct_opponent_neighbors[i]:
-        #         for opponent in opponents:
-        #             print(BoardHelper.calculate_direction(stone, opponent))
-
-        for i in range(len(current_player_stones)):
-            neighbors = current_player_stones[i]
-            # print(neighbors)
-            for j in direct_opponent_neighbors[i]:
-                print(BoardHelper.calculate_direction(neighbors, j))
-
+        #     neighbors = current_player_stones[i]
+        #     for j in direct_opponent_neighbors[i]:
+        #         print(BoardHelper.calculate_direction(neighbors, j))
 
         print("current player: " + current_player_stones.__str__())
         print("opponent stones: " + direct_opponent_neighbors.__str__())
@@ -74,23 +63,49 @@ class BoardHelper:
         return current_player_stones
 
     @staticmethod
-    def get_opponent_neighbors(board, x, y, opponent):
-        all_neighbors = BoardHelper.get_all_neighbors(board, x, y)
-        neighboring_opponent_stones = []
-        for neighbor in all_neighbors:
-            for row in board:
-                for column in row:
-                    if column == opponent and (board.index(row), row.index(column)) == neighbor:
-                        neighboring_opponent_stones.append((board.index(row), row.index(column)))
-        return neighboring_opponent_stones
+    def do_things_for_directions(board, own_stones, opponent):
+        for stone in own_stones:
+            for direction in BoardHelper.directions:
+                direction_x = stone[0] - direction[0]
+                direction_y = stone[1] - direction[1]
+
+                BoardHelper.weer_een_nieuwe(board, direction_x, direction_y, stone, opponent)
+
+
 
     @staticmethod
-    def get_all_neighbors(board, x, y):
+    def weer_een_nieuwe(board, direction_x, direction_y, start_stone, opponent):
+        empty_location = 0
+        if board[direction_x][direction_y]:
+            new_x = start_stone[0] - direction_x
+            new_y = start_stone[1] - direction_y
+
+            new_stone_x = 0
+            new_stone_y = 0
+
+
+            if BoardHelper.is_legal_direction(board, direction_x, direction_y, opponent):
+                new_stone_x =
+
+                new_stone = (new_stone_x, new_stone_y)
+                    BoardHelper.weer_een_nieuwe(board, direction_x, direction_y, new_stone, opponent)
+            else:
+                return False
+        return empty_location
+
+    @staticmethod
+    def calculate_direction(original, new):
+        x_direction = int(original[0]) - int(new[0])
+        y_direction = int(original[1]) - int(new[1])
+
+        return x_direction, y_direction
+
+    @staticmethod
+    def get_all_directions(board, x, y):
         neighbors = []
         length = len(board) - 1
 
-        positions = [(x - 1, y), (x, y - 1), (x, y + 1), (x + 1, y),
-                     (x - 1, y - 1), (x - 1, y + 1), (x + 1, y - 1), (x + 1, y + 1)]
+        positions = [(x - 1, y), (x, y - 1), (x, y + 1), (x + 1, y), (x - 1, y - 1), (x - 1, y + 1), (x + 1, y - 1), (x + 1, y + 1)]
         for pos in positions:
             if 0 <= pos[0] <= length and 0 <= pos[1] <= length:
                 neighbors.append((pos[0], pos[1]))
@@ -115,8 +130,9 @@ class BoardHelper:
         print('')
 
     @staticmethod
-    def is_legal_move(board, x, y):
-        if board[x][y] == Tile.EMPTY:
+    def is_legal_direction(board, x, y, opponent):
+        #mag niet leeg zijn en moet opponent zijn
+        if board[x][y] != Tile.EMPTY and board[x][y] == opponent:
             return True
         else:
             return False
