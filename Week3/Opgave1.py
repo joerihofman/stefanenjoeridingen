5d4d1b722b9f3a13fda431246bf86a2ef560a482
b0b123 <s.voogd@st.hanze.nl>
2018-10-01 12:35:37 +0200

    opgave 1 week 3 af

---
+++ Week3/Opgave1.py
@@ -0,0 +1,18 @@
+from itertools import permutations
+
+
+floors = (0, 1, 2, 3, 4)
+
+
+def puzzle(floors):
+    for (H, K, L, P, R) in list(permutations(floors)):
+        if H != 4:
+            if K != 0:
+                if L != 4 and L != 0:
+                    if P > K:
+                        if R - L != 1 and R - L != -1:
+                            if L - K != 1 and L - K != -1:
+                                return [H, K, L, P, R]
+
+list = puzzle(floors)
+print(list)
